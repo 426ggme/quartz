55929eab7d686c364a4806c98ae808f106e2bb79
Wataru Kajino <w.kajino2004@gmail.com>
2026-06-14 17:25:05 +0900

    fonts

--- quartz.ts
+++ quartz.ts
@@ -1,6 +1,7 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 import { registerCondition } from "./quartz/plugins/loader/conditions"
 import type { QuartzPluginData } from "./quartz/plugins/vfile"
+import ResponsiveToc from "./quartz/components/ResponsiveToc"
 import * as ExternalPlugin from "./.quartz/plugins"
 
 registerCondition("index-only", (props) => props.fileData.slug === "index")
@@ -15,4 +16,11 @@ ExternalPlugin.RecentNotes({
 
 const config = await loadQuartzConfig()
 export default config
-export const layout = await loadQuartzLayout()
+
+const loadedLayout = await loadQuartzLayout()
+const responsiveToc = ResponsiveToc(undefined)
+loadedLayout.defaults.left = [...(loadedLayout.defaults.left ?? []), responsiveToc]
+for (const pageLayout of Object.values(loadedLayout.byPageType)) {
+  pageLayout.left = [...(pageLayout.left ?? []), responsiveToc]
+}
+export const layout = loadedLayout
