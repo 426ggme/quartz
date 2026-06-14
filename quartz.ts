4f1e0bbfff177e7d70783446803294a4fe35bbfe
Wataru Kajino <w.kajino2004@gmail.com>
2026-06-14 13:38:53 +0900

    style

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,17 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { registerCondition } from "./quartz/plugins/loader/conditions"
+import type { QuartzPluginData } from "./quartz/plugins/vfile"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+registerCondition("index-only", (props) => props.fileData.slug === "index")
+
+ExternalPlugin.RecentNotes({
+  filter: (page: QuartzPluginData) =>
+    typeof page.filePath === "string" &&
+    page.filePath.endsWith(".md") &&
+    page.slug !== "index" &&
+    page.slug !== "all-pages",
+})
 
 const config = await loadQuartzConfig()
 export default config
